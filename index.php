4bea622caf5e2176325c427155ee59ab92ed6c85
LenanBagus <lenanbagus@outlook.com>
2022-01-03 22:57:26 +0700

    tes push to cpanel

--- index.php
+++ index.php
@@ -1,78 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<!-- meta's -->
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- load css -->
-		<link rel="stylesheet" href="assets/stylesheets/bootstrap.min.css">
-		<link rel="stylesheet" href="assets/stylesheets/custom.css">
+<head>
+	<!-- meta's -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- load javascript -->
-		<script src="assets/javascripts/jquery.slim.min.js"></script>
-		<script src="assets/javascripts/bootstrap.bundle.min.js"></script>
+	<!-- load css -->
+	<link rel="stylesheet" href="assets/stylesheets/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/stylesheets/custom.css">
 
-		<title>Beranda</title>
-	</head>
+	<!-- load javascript -->
+	<script src="assets/javascripts/jquery.slim.min.js"></script>
+	<script src="assets/javascripts/bootstrap.bundle.min.js"></script>
 
-	<body>
-		<?php
-			include 'menu.php';
-		?>
+	<title>Beranda</title>
+</head>
 
-		<div class="container-fluid">
-			<br>
-			<div class="card text-center shadow-lg">
-				<div class="card-header bg-success text-white">
-					<strong>Aplikasi Agenda Perjalanan Dinas</strong>
-				</div>
-				<div class="card-body bg-light">
+<body>
+	<?php
+	include 'menu.php';
+	?>
 
-					<div class="row">
-						<div class="col-sm-4">
-							<div class="card shadow-lg">
-								<div class="card-body">
-									<h5 class="card-title">Data Karyawan</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<a href="data_asn.php" class="btn btn-success">Register</a>
-								</div>
+	<div class="container-fluid">
+		<br>
+		<div class="card text-center shadow-lg">
+			<div class="card-header bg-success text-white">
+				<strong>Aplikasi AgendaAgendaAgendaAgendaAgenda Perjalanan Dinas</strong>
+			</div>
+			<div class="card-body bg-light">
+
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="card shadow-lg">
+							<div class="card-body">
+								<h5 class="card-title">Data Karyawan</h5>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<a href="data_asn.php" class="btn btn-success">Register</a>
 							</div>
 						</div>
-						<div class="col-sm-4">
-							<div class="card shadow-lg">
-								<div class="card-body">
-									<h5 class="card-title">Agenda Perjalanan</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<a href="data_agenda.php" class="btn btn-success">Buat Agenda</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="card shadow-lg">
-								<div class="card-body">
-									<h5 class="card-title">Notulen Rapat / Bimtek</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<a href="#" class="btn btn-success">Buat Notulen</a>
-								</div>
-							</div>
-						</div>
-
 					</div>
-					<br><br><br><br>
-					<h5 class="card-title">Today Quote</h5>
-					<blockquote class="card-text">
-						<p class="mb-0">All journeys have secret destinations of which the traveler is unaware.</p>
-						<footer class="blockquote-footer">Martin Buber <cite title="Source Title"></cite></footer>
-					</blockquote>
-					<!-- <a href="#" class="btn btn-success">Simpan</a>
-						<a href="#" class="btn btn-dark">Kembali</a> -->
+					<div class="col-sm-4">
+						<div class="card shadow-lg">
+							<div class="card-body">
+								<h5 class="card-title">Agenda Perjalanan</h5>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<a href="data_agenda.php" class="btn btn-success">Buat Agenda</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="card shadow-lg">
+							<div class="card-body">
+								<h5 class="card-title">Notulen Rapat / Bimtek</h5>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<a href="#" class="btn btn-success">Buat Notulen</a>
+							</div>
+						</div>
+					</div>
+
 				</div>
+				<br><br><br><br>
+				<h5 class="card-title">Today Quote</h5>
+				<blockquote class="card-text">
+					<p class="mb-0">All journeys have secret destinations of which the traveler is unaware.</p>
+					<footer class="blockquote-footer">Martin Buber <cite title="Source Title"></cite></footer>
+				</blockquote>
+				<!-- <a href="#" class="btn btn-success">Simpan</a>
+						<a href="#" class="btn btn-dark">Kembali</a> -->
 			</div>
 		</div>
-		<?php
-		include 'footer.php';
-		?>
-	</body>
+	</div>
+	<?php
+	include 'footer.php';
+	?>
+</body>
+
 </html>
