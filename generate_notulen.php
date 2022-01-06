ce91c13625a84f00b0ac97f77d0740bd44d1f5eb
LenanBagus <lenanbagus@outlook.com>
2022-01-06 23:36:55 +0700

    adding file input

--- generate_notulen.php
+++ generate_notulen.php
@@ -4,5 +4,11 @@
   </div>
   <div class="col-9 pl-0">
     <textarea class="form-control" rows="5" id="isi_notulen" name="isi_notulen"></textarea>
+    <br>
+    <label for="foto1">Unggah Foto Kegiatan</label>
+    <br><br>
+    <input type="file" name="foto1" id="foto1">
+    <br><br>
+    <input type="file" name="foto2" id="foto2">
   </div>
 </div>
